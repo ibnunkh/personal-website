45bf07c111e1207ccf3a0fa8402652159238280f
ibnunkh <ibnunashirk@gmail.com>
2025-05-21 14:54:40 +0700

    menambahkan project portofolio baru

--- src/components/organisems/ProjectsCard.jsx
+++ src/components/organisems/ProjectsCard.jsx
@@ -1,5 +1,6 @@
 import styles from '../../styles/styles.module.css';
 import ssTodolist from '../../assets/images/Cuplikan layar 2025-05-02 163837.png';
+import ssCafe from '../../assets/images/Cuplikan layar 2025-05-21 144810.png';
 import Heading3 from '../atoms/Heading3';
 
 const ProjectsCard = () => {
@@ -10,6 +11,13 @@ const ProjectsCard = () => {
                 <img src={ssTodolist} alt="todolistapp" />
                 <p className={styles.projectCategory}>Productivity App</p>
                 <Heading3 className={styles.projectName}>Product To Do List App</Heading3>
+                </a>
+            </div>
+            <div className={styles.projectCard}>
+                <a href="https://nongkrongbandung.vercel.app/" target="_blank" rel="noopener noreferrer">
+                <img src={ssCafe} alt="todolistapp" />
+                <p className={styles.projectCategory}>landing Page</p>
+                <Heading3 className={styles.projectName}>Product Cafe Mapping in Bandung</Heading3>
                 </a>
             </div>
         </>
